826d69667176822518fa8e2fcaa483ad3dd143af
Vicente Redruello Rossi <vicenteredruello@gmail.com>
2026-03-20 20:03:51 -0300

    Feat(Multiple Rounds): Ask the user if he wants to play again, and prevent the word to be one that has already been pulled before. If the category was cleared, ask the user to select a category again

--- questions.py
+++ questions.py
@@ -29,91 +29,117 @@ categories = {
         "rojo",
         "azul",
         "verde",
-        "violeta",
-        "magenta",
-        "amarillo",
-        "naranja",
-        "marron",
-        "turquesa",
-        "cian",
-        "celeste",
+        # "violeta",
+        # "magenta",
+        # "amarillo",
+        # "naranja",
+        # "marron",
+        # "turquesa",
+        # "cian",
+        # "celeste",
     ],
 }
 
-selected_category = False
-words = False
-while not words:
-    print("Elija una de las categorias")
-    for i, cat in enumerate(categories.keys()):
-        print(f"{i}) {cat}")
+def menu(options: list[str]):
+    while True:
+        print("Elija una de las siguientes opciones")
+        print()
+        for i, opt in enumerate(options):
+            print(f"{i}) {opt}")
+        print()
 
-    user_category_input = input("Categoria: ")
+        user_input = input("Opcion: ")
+        print()
 
-    if user_category_input in categories.keys():
-        selected_category = user_category_input
-        words = categories[user_category_input]
-        break
+        if user_input in options:
+            return user_input
 
-    try:
-        int_user_category_input = int(user_category_input)
-        if (int_user_category_input >= 0) and (int_user_category_input < len(categories.keys())):
-            selected_category = [c for c in categories.keys()][int_user_category_input]
-            words = categories[selected_category]
-            break
-        print("No existe la categoria ingresada")
-    except:
-        print("No existe la categoria ingresada")
+        try:
+            int_user_input = int(user_input)
+            if (int_user_input >= 0) and (int_user_input < len(options)):
+                return options[int_user_input]
+            print("No existe la opcion ingresada")
+        except:
+            print("No existe la opcion ingresada")
+        
+        print()
 
-print(f"Elejiste la categoria {selected_category}")
-
-word = random.choice(words)
-guessed = []
-attempts = 6
-points = 0
 
 print("¡Bienvenido al Ahorcado!")
 print()
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
-        else:
-            progress += "_ "
+words = []
 
-    print(progress)
+while True:
+    if not len(words) > 0:
+        selected_category = menu(list(categories.keys()))
+        print(f"Elejiste la categoria {selected_category}")
+        print()
+        words = categories[selected_category].copy()
+        random.shuffle(words)
+        category_points = 0
+        category_victories = 0
 
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        points += 6
-        break
+    word = words.pop()
 
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-
-    letter = input("Ingresá una letra: ")
-
-    if len(letter) > 1 or len(letter) == 0 or letter < 'A' or letter > 'z':
-        print("Entrada no válida")
-        continue
-
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
-    else:
-        guessed.append(letter)
-        points -= 1
-        attempts -= 1
-        
-    print("Esa letra no está en la palabra.")
-    print()
-else:
+    guessed = []
+    attempts = 6
     points = 0
-    print(f"¡Perdiste! La palabra era: {word}")
 
-print(f"Terminaste con {points} puntos!")
+    while attempts > 0:
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+
+        print(progress)
+
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            points += 6
+            category_victories += 1
+            break
+
+        print(f"Intentos restantes: {attempts}")
+        print(f"Letras usadas: {', '.join(guessed)}")
+
+        letter = input("Ingresá una letra: ")
+
+        if len(letter) > 1 or len(letter) == 0 or letter < 'A' or letter > 'z':
+            print("Entrada no válida")
+            continue
+
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            points -= 1
+            attempts -= 1
+            
+        print("Esa letra no está en la palabra.")
+        print()
+    else:
+        points = 0
+        print(f"¡Perdiste! La palabra era: {word}")
+
+    category_points += points
+
+    print(f"Terminaste con {points} puntos!")
+    print()
+
+    if not len(words) > 0:
+        print(f"Terminaste la categoria {selected_category} con {category_victories} victorias y {category_points} puntos!")
+        print()
+
+    print('Te gustaria jugar de nuevo?')
+    play_again = menu(['si', 'no'])
+    if play_again != 'si':
+        print('Chau!')
+        break
